ec8e40b2dbb2fca950e4891947cd678052cd760e
dipanshub22 <dipanshuborkar22@gmail.com>
2026-03-25 13:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square =>{
+	square.addEventListener('mouseover',()=>{
+		squares.forEach(sq =>{
+			if(sq !==square){
+				sq.style.backgroundColor ='#6F4E3';
+			} else{
+				sq.style.backgroundColor ='#E6E6FA';
+			}
+		});
+	});
+
+	square.addEventListener('mouseout', ()=>{
+		squares.forEach(sq=>{
+			sq.style.backgroundColor ='E6E6FA';
+		})
+	})
+	
+})
